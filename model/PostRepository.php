15491706d6587462c430dec82e8f3ffb49c98715
democvidev <cnvvc_vlad@mail.ru>
2021-06-12 21:58:47 +0200

    create a variable for db connection and add constructor

--- model/PostRepository.php
+++ model/PostRepository.php
@@ -5,21 +5,38 @@ require_once 'AbstractRepository.php';
 
 class PostRepository extends AbstractRepository
 {
+    // dès qu'on crée le model des Posts on met en place un variable de connexion à la bdd, en utilisant le constructeur
+    private $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = $this->getDBConnection();
+    }
+
+    /**
+     * Retourne une liste des articles
+     *
+     * @return array
+     */
     public function findAll(): array
     {
-        $dbh = $this->getDBConnection();
         $query = 'SELECT posts.id AS id, title, LEFT(content, 150) AS content, DATE_FORMAT(date, \'%d/%m/%Y à %Hh%imin%ss\') AS date, users.name AS user FROM posts INNER JOIN users ON posts.user_id = users.id';
-        $req = $dbh->query($query);
+        $req = $this->dbh->query($query);
         $posts = $req->fetchAll();
         $req->closeCursor();
         return $posts;
     }
 
+    /**
+     * Retourne un seul article
+     *
+     * @param integer $id
+     * @return array
+     */
     public function findOneById(int $id): array
     {
-        $dbh = $this->getDBConnection();
         $query = 'SELECT posts.id AS id, title, content, DATE_FORMAT(date, \'%d/%m/%Y à %Hh %imin %ss\') AS date, users.name AS user FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.id =:id;';
-        $req = $dbh->prepare($query);
+        $req = $this->dbh->prepare($query);
         $req->bindValue('id', $id, PDO::PARAM_INT);
         $req->execute();
         $post = $req->fetch();
@@ -30,12 +47,17 @@ class PostRepository extends AbstractRepository
         return $post;
     }
 
+    /**
+     * Supprime un article
+     *
+     * @param integer $id
+     * @return void
+     */
     public function deleteOne(int $id): void
     {
-        $dbh = $this->getDBConnection();
         $this->findOneById($id); //vérification si l'article est dans la bdd
         $query = 'DELETE FROM posts WHERE id = :id;';
-        $req = $dbh->prepare($query);
+        $req = $this->dbh->prepare($query);
         $req->bindValue('id', $id, PDO::PARAM_INT);
         $req->execute();
         $req->closeCursor();
--- model/UserRepository.php
+++ model/UserRepository.php
@@ -4,12 +4,18 @@ require_once 'AbstractRepository.php';
 
 class UserRepository extends AbstractRepository
 {
+
+    private $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = $this->getDBConnection();
+    }
+
     public function insertOne(array $data): void
     {
-        // var_dump($data); die;
-        $dbh = $this->getDBConnection();
         $query = 'INSERT INTO users(name, email, password, created_at) VALUES(:name, :email, :password, Now());';
-        $req = $dbh->prepare($query);
+        $req = $this->dbh->prepare($query);
         $req->bindValue('name', $data['name'], PDO::PARAM_STR);
         $req->bindValue('email', $data['email'], PDO::PARAM_STR);
         $req->bindValue('password', $data['password'], PDO::PARAM_STR);
