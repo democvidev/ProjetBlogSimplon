8a92621f16aa9fecb62b792e86f04c0e9b593025
democvidev <cnvvc_vlad@mail.ru>
2021-06-13 22:47:17 +0200

    add singleton pattern for database connexion

--- model/DataBase.php
+++ model/DataBase.php
@@ -4,6 +4,8 @@ namespace App\Model;
 
 abstract class DataBase
 {
+    private static $instace = null; // Pattern singleton permet de se connecter une seule fois à MySQL en stockant la connexion dans la variable $instance
+
     // Couche d'accès au données
     private $user = "root";
     private $pass = "";
@@ -22,19 +24,23 @@ abstract class DataBase
 
     protected function getDBConnection(): \PDO
     {
-        // Mise en place d'un système de déroutage et gestion d'exceptions
+        if (self::$instace === null) {
 
-        try {
-            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
-            $options = [
-        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
-    ];
-            return new \PDO($dsn, $this->user, $this->pass, $options);
-        } catch (\PDOException $e) {
-            print "Erreur !: " . $e->getMessage() . "<br/>";
-            die();
+            // Mise en place d'un système de déroutage et gestion d'exceptions
+            
+            try {
+                $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+                $options = [
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+                ];
+                self::$instace = new \PDO($dsn, $this->user, $this->pass, $options);
+            } catch (\PDOException $e) {
+                print "Erreur !: " . $e->getMessage() . "<br/>";
+                die();
+            }
         }
+        return self::$instace;
     }
 }
