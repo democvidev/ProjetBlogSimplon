53de8ecf5e33e38411477b7d8a9bb12ee469b6b6
Vladislav <cnvvc_vlad@mail.ru>
2021-10-27 21:12:06 +0200

    add styles

--- view/base.php
+++ view/base.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/journal/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/css/style.css">
 </head>
+
 <body>
-    <?php require 'shared/_nav.php'; ?>
-    <div class="container">
-    <?= $content; ?>
+    <div class="main" id="mainContent">
+        <!-- Le cas où l'utilisateur n'a pas de JavaScript sur le navigateur -->
+        <script>
+            document.getElementById('mainContent').classList.add('isLoading')
+        </script>
+        <?php require 'shared/_nav.php'; ?>
+        <div class="container">
+            <?= $content ?>
+        </div>
     </div>
+    <script src="js/script.js" defer></script>
 </body>
+
 </html>
--- view/posts/home.php
+++ view/posts/home.php
@@ -1,26 +1,24 @@
-<div class="row">
-    <?php foreach ($posts as $post): ?>
+<!-- Loader pour la page d'accueil -->
+<div class="loader"></div>
+<div class="row home">
+    <?php foreach ($posts as $post) : ?>
         <div class="col-3">
             <div class="card border-dark mb-3" style="max-width: 20rem;">
-            <div class="card-header"><?= htmlspecialchars(
-                $post['user']
-            ) ?>, le <?= $post['date'] ?></div>
-            <div class="card-body">
-                <h4 class="card-title"><?= htmlspecialchars(
-                    $post['art_title']
-                ) ?></h4>
-                <p class="card-text"><?= nl2br(
-                    htmlspecialchars($post['description'])
-                ) . '...' ?></p>
-                <p class="d-flex justify-content-between">
-                    <a href="?page=post.show&amp;id=<?= $post[
-                        'id'
-                    ] ?>">Voir plus ...</a>
-                    <a href="?page=post.delete&amp;id=<?= $post[
-                        'id'
-                    ] ?>">Supprimer</a>
-                </p>                
-            </div>
+                <div class="card-header"><?= htmlspecialchars(
+                                                $post['user']
+                                            ) ?>, le <?= $post['date'] ?></div>
+                <div class="card-body">
+                    <h4 class="card-title"><?= htmlspecialchars(
+                                                $post['art_title']
+                                            ) ?></h4>
+                    <p class="card-text"><?= nl2br(
+                                                htmlspecialchars($post['description'])
+                                            ) . '...' ?></p>
+                    <p class="d-flex justify-content-between">
+                        <a href="?page=post.show&amp;id=<?= $post['id'] ?>">Voir plus ...</a>
+                        <a href="?page=post.delete&amp;id=<?= $post['id'] ?>">Supprimer</a>
+                    </p>
+                </div>
             </div>
         </div>
     <?php endforeach; ?>
