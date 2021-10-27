0772c43ec5becc0083b32cbc2bb84a6dbd9b3c43
Vladislav <cnvvc_vlad@mail.ru>
2021-10-27 23:02:01 +0200

    refactoring models

--- model/DataBase.php
+++ model/DataBase.php
@@ -2,45 +2,7 @@
 
 namespace App\Model;
 
-abstract class DataBase
+interface DataBase
 {
-    private static $instace = null; // Pattern singleton permet de se connecter une seule fois à MySQL en stockant la connexion dans la variable $instance
-
-    // Couche d'accès au données
-    private $user = "root";
-    private $pass = "";
-    private $dbname = "bd_blog_simplon";
-    private $host = 'localhost';
-
-    public function __construct($host=null, $user=null, $pass=null, $dbname=null)
-    {
-        if ($host != null) {
-            $this->host = $host;
-            $this->user = $user;
-            $this->pass = $pass;
-            $this->dbname = $dbname;
-        }
-    }
-
-    protected function getDBConnection(): \PDO
-    {
-        if (self::$instace === null) {
-
-            // Mise en place d'un système de déroutage et gestion d'exceptions
-            
-            try {
-                $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
-                $options = [
-                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
-                ];
-                self::$instace = new \PDO($dsn, $this->user, $this->pass, $options);
-            } catch (\PDOException $e) {
-                print "Erreur !: " . $e->getMessage() . "<br/>";
-                die();
-            }
-        }
-        return self::$instace;
-    }
+    public function getDBConnection(): \PDO;
 }
--- model/Repository.php
+++ model/Repository.php
@@ -1,20 +1,59 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Model;
 
 use App\Model\DataBase;
 
 // require_once 'DataBase.php';
 
-class Repository extends DataBase
+abstract class Repository implements DataBase
 {
     // dès qu'on crée le model des Posts on met en place un variable de connexion à la bdd, en utilisant le constructeur. Elle sera réservée à Repository et ses enfants
     protected $dbh;
     protected $table; // on peut réutiliser les requêtes en chagent le nom de la table
 
-    public function __construct()
+    private static $instace = null; // Pattern singleton permet de se connecter une seule fois à MySQL en stockant la connexion dans la variable $instance
+
+    // Couche d'accès au données
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "bd_blog_simplon";
+    private $host = 'localhost';
+
+    public function __construct($host=null, $user=null, $pass=null, $dbname=null)
     {
+        if ($host != null) {
+            $this->host = $host;
+            $this->user = $user;
+            $this->pass = $pass;
+            $this->dbname = $dbname;
+        }
+
         $this->dbh = $this->getDBConnection();
+    }
+
+    public function getDBConnection(): \PDO
+    {
+        if (self::$instace === null) {
+
+            // Mise en place d'un système de déroutage et gestion d'exceptions
+            
+            try {
+                $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+                $options = [
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+                ];
+                self::$instace = new \PDO($dsn, $this->user, $this->pass, $options);
+            } catch (\PDOException $e) {
+                print "Erreur !: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+        return self::$instace;
     }
 
     /**
