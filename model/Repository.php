a2e4072d4d6f152953632ca2df875fcb007a5959
Vladislav <cnvvc_vlad@mail.ru>
2021-10-27 23:37:07 +0200

    refactoring & updated constants

--- model/Repository.php
+++ model/Repository.php
@@ -10,46 +10,32 @@ use App\Model\DataBase;
 
 abstract class Repository implements DataBase
 {
-    // dès qu'on crée le model des Posts on met en place un variable de connexion à la bdd, en utilisant le constructeur. Elle sera réservée à Repository et ses enfants
+    // dès qu'on crée le model de Repository on met en place un variable de connexion à la bdd, en utilisant le constructeur. Elle sera réservée à Repository et ses enfants
     protected $dbh;
     protected $table; // on peut réutiliser les requêtes en chagent le nom de la table
 
     private static $instace = null; // Pattern singleton permet de se connecter une seule fois à MySQL en stockant la connexion dans la variable $instance
 
-    // Couche d'accès au données
-    private $user = "root";
-    private $pass = "";
-    private $dbname = "bd_blog_simplon";
-    private $host = 'localhost';
-
-    public function __construct($host=null, $user=null, $pass=null, $dbname=null)
+    public function __construct()
     {
-        if ($host != null) {
-            $this->host = $host;
-            $this->user = $user;
-            $this->pass = $pass;
-            $this->dbname = $dbname;
-        }
-
         $this->dbh = $this->getDBConnection();
     }
 
     public function getDBConnection(): \PDO
     {
         if (self::$instace === null) {
-
             // Mise en place d'un système de déroutage et gestion d'exceptions
-            
+
             try {
-                $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+                $dsn = 'mysql:host=' . SERVER . ';dbname=' . DB_NAME;
                 $options = [
-                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
                     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 ];
-                self::$instace = new \PDO($dsn, $this->user, $this->pass, $options);
+                self::$instace = new \PDO($dsn, USER, PASSWD, $options);
             } catch (\PDOException $e) {
-                print "Erreur !: " . $e->getMessage() . "<br/>";
+                print 'Erreur !: ' . $e->getMessage() . '<br/>';
                 die();
             }
         }
